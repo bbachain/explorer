4033464fe51e410bbb7601829e82e4b1ef17c1c4
Dev Cresix <52518140+devcresix@users.noreply.github.com>
2023-07-20 12:15:43 +0530

    home page tx,bl=10

--- src/components/home/LatestTxs.tsx
+++ src/components/home/LatestTxs.tsx
@@ -64,7 +64,7 @@ export const LatestTxs: FC = ({}) => {
               </tr>
             </thead>
             <tbody>
-              {transactions.slice(0, 3).map((transactionData) => {
+              {transactions.slice(0, 10).map((transactionData) => {
                 return (
                   <tr key={transactionData.signature}>
                     <td>
